64740275569403b224cfa02398def3f9c05337ca
xld <1097551561@qq.com>
2021-04-14 20:11:15 +0800

    2021-4-14 20:11

--- 非递减数列.cpp
+++ 非递减数列.cpp
@@ -9,15 +9,20 @@ public:
         int change = 0;
         for(int i = 0; i < nums.size()-1; i++){
             if( i == 0 && nums[i] > nums[i + 1]){//第一个位置单独讨论
+                //前面没有数字，直接改为后面
                 nums[i] = nums[i + 1];
                 change ++;
             }
             else if(nums[i] > nums[i + 1]){
+                
                 change ++;
+                //前面有数字，判断前面数字是否大于后面数字
                 if(nums[i + 1] >= nums[i - 1]){
+                    //前面数字小于后面数字
                     nums[i] = nums[i + 1];
                 }
                 else{
+                    //前面数字大于后面数字，
                     nums[i + 1] = nums[i];
                 }
             }
