8109a41e3f5ea9cd95acf2a4c2397dd439a21793
1097551561@qq.com <1097551561@qq.com>
2021-04-15 17:43:14 +0800

    2021-4-15 17:43

---
+++ 打家劫舍2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int robRange(vector<int>& nums, int start, int end) {
+        int first = nums[start], second = max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+    int rob(vector<int>& nums) {
+        int length = nums.size();
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return max(nums[0], nums[1]);
+        }
+        return max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+    }
+
+
+};
+
+
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+    vector<int> nums = {200,3,140,20,10};
+    cout << solution.rob(nums);
+    return 0;
+}
+
